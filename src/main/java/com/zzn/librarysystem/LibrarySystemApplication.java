5a307c02d7865e31187efd942c91736569edba05
zhang <2322705031@qq.com>
2024-09-10 22:41:56 +0800

    'init'

---
+++ src/main/java/com/zzn/librarysystem/LibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.zzn.librarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySystemApplication.class, args);
+    }
+
+}
